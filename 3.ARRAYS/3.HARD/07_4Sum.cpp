ff5fcced9ca5b0ee66fb2a93754c6b7f135f3ffd
Prabhat <osrprabhatyadav4@gmail.com>
2026-02-03 18:15:29 +0530

    Arrays: added better & optimal solution for four sum.

--- 3.ARRAYS/3.HARD/07_4Sum.cpp
+++ 3.ARRAYS/3.HARD/07_4Sum.cpp
@@ -58,6 +58,60 @@ public:
         vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
+
+    vector<vector<int>> betterFourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> st;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                set<long long> hashset;
+                for(int k = j+1; k < n; k++){
+                    long long sum = nums[i] + nums[j] + nums[k];
+                    long long fourth = target - (sum);
+                    if(hashset.find(fourth) != hashset.end()){
+                        vector<int> temp = {nums[i], nums[j], nums[k], (int)fourth};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                    hashset.insert(nums[k]);
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+
+    vector<vector<int>> optimalFourSum(vector<int>& nums, int target){
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1])   continue;
+            for(int j = i+1; j < n; j++){
+                if(j > (i+1) && nums[j] == nums[j-1])   continue;
+                int k = j+1;
+                int l = n-1;
+                while(k < l){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++, l--;
+                        while(k < l && nums[k] == nums[k-1])    k++;
+                        while(k < l && nums[l] == nums[l+1])    l--;
+                    }
+                    else if(sum < target)   k++;
+                    else   l--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
 };
 
 int main() {
@@ -68,6 +122,24 @@ int main() {
     vector<vector<int>> res1 = obj.bruteFourSum(arr, 0);
     cout << "The Four Elements (Brute) are: " << endl;;
     for(auto &row : res1){
+        for(auto &val : row){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    vector<vector<int>> res2 = obj.betterFourSum(arr, 0);
+    cout << "\nThe Four Elements (Better) are: " << endl;
+    for(auto &row : res2){
+        for(auto &val : row){
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    vector<vector<int>> res3 = obj.optimalFourSum(arr, 0);
+    cout << "\nThe Four Elements (Optimal) are: " << endl;
+    for(auto &row : res3){
         for(auto &val : row){
             cout << val << " ";
         }
