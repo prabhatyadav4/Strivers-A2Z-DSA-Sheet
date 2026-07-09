962572284220690cb9a69496cbd27e2ade288199
Prabhat <osrprabhatyadav4@gmail.com>
2026-07-09 23:03:13 +0530

    feat: add brute force implementation for Largest Rectangle in Histogram and executable

--- 9.STACK AND QUEUES/NOTES/20_largest_rectangle_in_histogram.cpp
+++ 9.STACK AND QUEUES/NOTES/20_largest_rectangle_in_histogram.cpp
@@ -1,7 +1,7 @@
 /*
 84. Largest Rectangle in Histogram
 
-Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram. 
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
 
 Example 1:
 
@@ -13,10 +13,78 @@ The largest rectangle is shown in the red area, which has an area = 10 units.
 Example 2:
 Input: heights = [2,4]
 Output: 4
- 
+
 
 Constraints:
 1 <= heights.length <= 105
 0 <= heights[i] <= 104
 */
 
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int BruteLargestRectangleArea(vector<int> &heights)
+{
+
+    int n = heights.size();
+    vector<int> right(n);
+    vector<int> left(n);
+    stack<int> st;
+
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && heights[st.top()] > heights[i])
+        {
+            right[st.top()] = i;
+            st.pop();
+        }
+
+        st.push(i);
+    }
+
+    while (!st.empty())
+    {
+        right[st.top()] = n;
+        st.pop();
+    }
+
+    // compute previous smaller element indices
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!st.empty() && heights[st.top()] > heights[i])
+        {
+            left[st.top()] = i;
+            st.pop();
+        }
+
+        st.push(i);
+    }
+
+    while (!st.empty())
+    {
+        left[st.top()] = -1;
+        st.pop();
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        ans = max(ans, heights[i] * (right[i] - left[i] - 1));
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+
+    int ans = BruteLargestRectangleArea(heights);
+
+    cout << "BRUTE: The area of the largest rectangle in the histogram: " << ans;
+
+    return 0;
+}
