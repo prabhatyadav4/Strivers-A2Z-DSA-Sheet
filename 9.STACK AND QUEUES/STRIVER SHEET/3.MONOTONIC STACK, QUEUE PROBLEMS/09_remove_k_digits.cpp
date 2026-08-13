d9e8ef1d3a89191d2eb9a46cc44aa52c7f91cc1a
Prabhat <osrprabhatyadav4@gmail.com>
2026-08-14 02:00:08 +0530

    feat: add implementation for Remove K Digits problem

---
+++ 9.STACK AND QUEUES/STRIVER SHEET/3.MONOTONIC STACK, QUEUE PROBLEMS/09_remove_k_digits.cpp
@@ -0,0 +1,75 @@
+/*
+402. Remove K Digits
+
+Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
+
+Example 1:
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+
+Example 2:
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+
+Example 3:
+Input: num = "10", k = 2
+Output: "0"
+Explanation: Remove all the digits from the number and it is left with nothing which is 0.
+
+
+Constraints:
+1 <= k <= num.length <= 105
+num consists of only digits.
+num does not have any leading zeros except for the zero itself.
+*/
+
+#include <iostream>
+using namespace std;
+
+string removeKdigits(string num, int k)
+{
+    int n = num.length();
+
+    string result = "";
+
+    for (int i = 0; i < n; i++)
+    {
+
+        while (result.length() > 0 && result.back() > num[i] && k > 0)
+        {
+            result.pop_back();
+            k--;
+        }
+
+        if (result.length() > 0 || num[i] != '0')
+        {
+            result.push_back(num[i]);
+        }
+    }
+
+    while (result.length() > 0 && k > 0)
+    {
+        result.pop_back();
+        k--;
+    }
+
+    if (result == "")
+    {
+        return "0";
+    }
+
+    return result;
+}
+
+int main()
+{
+    string num = "1432219";
+    int k = 3;
+
+    string ans = removeKdigits(num, k);
+    cout << "The smallest possible integer after removing k digits is: " << ans;
+
+    return 0;
+}
