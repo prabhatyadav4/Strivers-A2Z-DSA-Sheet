1677b0be66f561f109c50613fe48406c293f1375
Prabhat <osrprabhatyadav4@gmail.com>
2026-09-05 10:29:00 +0530

    feat: add implementation for Queue using Stacks

---
+++ 9.STACK AND QUEUES/STRIVER SHEET/1.LEARNING/08_implement_queue_using_stacks.cpp
@@ -0,0 +1,118 @@
+/*
+232. Implement Queue using Stacks
+
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+
+
+Constraints:
+1 <= x <= 9
+At most 100 calls will be made to push, pop, peek, and empty.
+All the calls to pop and peek are valid.
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Queue
+{
+    stack<int> s1, s2;
+
+public:
+    void enqueue(int x)
+    {
+        s1.push(x);
+    }
+
+    void dequeue()
+    {
+        if (s1.empty() && s2.empty())
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        if (s2.empty())
+        {
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        s2.pop();
+    }
+
+    int front()
+    {
+        if (s1.empty() && s2.empty())
+        {
+            cout << "Queue is empty\n";
+            return -1;
+        }
+
+        if (s2.empty())
+        {
+            while (!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        return s2.top();
+    }
+
+    bool empty()
+    {
+        return s1.empty() && s2.empty();
+    }
+};
+
+int main()
+{
+    Queue q;
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    cout << q.front() << endl; // 10
+
+    q.dequeue();
+
+    cout << q.front() << endl; // 20
+
+    q.dequeue();
+
+    cout << q.front() << endl; // 30
+}
